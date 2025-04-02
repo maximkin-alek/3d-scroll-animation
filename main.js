8b8292d09453e0fa9d85f29f7acab0592570a54d
Alexander Maximkin <maximkin.alek@yandex.ru>
2025-04-02 13:16:46 +0300

    fix: touch devices scroll

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let lastPos = zSpacing / 5;
 const $frames = document.getElementsByClassName('frame');
 const frames = Array.from($frames);
 const zVals = [];
+const isTouchDevice = 'ontouchstart' in window;
 
 console.log(frames);
 window.onscroll = function () {
@@ -12,11 +13,13 @@ window.onscroll = function () {
   let delta = lastPos - top;
   lastPos = top;
 
-  console.log(delta);
+  if (isTouchDevice) {
+    delta = -delta;
+  }
 
   frames.forEach((n, i) => {
     zVals.push(i * zSpacing + zSpacing);
-    zVals[i] += delta * -3;
+    zVals[i] += delta * (isTouchDevice ? 3 : -3); // Для тачскринов используем 3, для десктопов -3
     let frame = frames[i];
     let transform = `translateZ(${zVals[i]}px)`;
     let opacity = zVals[i] < Math.abs(zSpacing / 1.5) ? 1 : 0;
